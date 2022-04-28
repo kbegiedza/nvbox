bb02366493a9a7b6b24ef0fa73d213fbbd6ff27e
Krzysztof Begiedza <contact@kbegiedza.eu>
2022-04-28 19:29:25 +0000

    Fetch GPU UUID

--- src/main.cpp
+++ src/main.cpp
@@ -71,68 +71,70 @@ int main(int argc, char *argv[])
 
     nvmlInit_v2();
 
-    while (true)
+    uint32_t deviceCount = 0;
+
+    nvmlReturn_t nvmlOpStatus;
+    nvmlOpStatus = nvmlDeviceGetCount_v2(&deviceCount);
+
+    if (nvmlOpStatus != nvmlReturn_t::NVML_SUCCESS)
     {
-        clear_console();
+        std::cerr << "Unable to query device count\n"
+                  << nvmlOpStatus
+                  << std::endl;
 
-        std::cout << "1" << std::endl;
+        std::exit(1);
+    }
 
-        uint32_t deviceCount = 0;
+    if (deviceCount <= 0)
+    {
+        std::cerr << "Unable to find any device"
+                  << std::endl;
 
-        nvmlReturn_t nvmlOpStatus;
-        nvmlOpStatus = nvmlDeviceGetCount_v2(&deviceCount);
+        std::exit(1);
+    }
 
+    std::vector<nvmlDevice_t> devices;
+
+    for (uint32_t deviceId = 0; deviceId < deviceCount; ++deviceId)
+    {
+        nvmlDevice_t currentDevice;
+        nvmlOpStatus = nvmlDeviceGetHandleByIndex_v2(deviceId, &currentDevice);
         if (nvmlOpStatus != nvmlReturn_t::NVML_SUCCESS)
         {
-            std::cerr << "Unable to query device count\n"
+            std::cerr << "Unable to get handle to device"
+                      << deviceId
+                      << std::endl
                       << nvmlOpStatus
                       << std::endl;
 
             break;
         }
 
-        if (deviceCount <= 0)
-        {
-            std::cerr << "Unable to find any device"
-                      << std::endl;
+        devices.push_back(std::move(currentDevice));
+    }
 
-            break;
-        }
-
-        std::vector<nvmlDevice_t> devices;
-
-        for (uint32_t deviceId = 0; deviceId < deviceCount; ++deviceId)
-        {
-            nvmlDevice_t currentDevice;
-            nvmlOpStatus = nvmlDeviceGetHandleByIndex_v2(deviceId, &currentDevice);
-            if (nvmlOpStatus != nvmlReturn_t::NVML_SUCCESS)
-            {
-                std::cerr << "Unable to get handle to device"
-                          << deviceId
-                          << std::endl
-                          << nvmlOpStatus
-                          << std::endl;
-
-                break;
-            }
-
-            devices.push_back(std::move(currentDevice));
-        }
+    while (true)
+    {
+        clear_console();
 
         for (auto &&device : devices)
         {
-
             uint32_t temp, temp_count;
 
+            char *deviceUUIDPtr = new char[NVML_DEVICE_UUID_V2_BUFFER_SIZE];
+
+            nvmlDeviceGetUUID(device, deviceUUIDPtr, NVML_DEVICE_UUID_V2_BUFFER_SIZE);
+
             nvmlDeviceGetTemperature(device, nvmlTemperatureSensors_t::NVML_TEMPERATURE_GPU, &temp);
-            nvmlDeviceGetTemperature(device, nvmlTemperatureSensors_t::NVML_TEMPERATURE_COUNT, &temp_count);
 
-            std::cout << "Device" << device << " " << temp << " " << temp_count << std::endl;
+            std::cout << "Device: " << deviceUUIDPtr
+                      << "\n=========================\n"
+                      << "Temp:\t" << temp << "°C"
+                      << std::endl;
+
+            delete[] deviceUUIDPtr;
+            deviceUUIDPtr = nullptr;
         }
-
-        // nvmlDeviceGetHandleByIndex_v2();
-
-        // nvmlTemperature();
 
         std::this_thread::sleep_for(sleepTime);
     }
